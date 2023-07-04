95bcf3dd86ee231f539fcb794eedc902e94bc81c
Gareth Reid <81276892+gareth-reid07@users.noreply.github.com>
2023-07-04 13:12:05 -0400

    Completed powershell Training

--- Mircorsoft Training Lessons/Part One/Backup.ps1
+++ Mircorsoft Training Lessons/Part One/Backup.ps1
@@ -3,12 +3,29 @@
 Param(
     [string]$Path = './app',
     [string]$DestinationPath = './',
-)
+    [switch]$PathIsWebApp
+    )
+#The code block below checks if the pathiswebapp is provided at runtime. if it is, the code continues to execute and grab a list of file extensions within the path
+If($PathIsWebApp -eq $True){
+    Try
+    {
+        $ContainsApplicationFiles = "$((Get-ChildItem $Path).Extension | Sort-Object -Unique)" -match '\.js|\.html|\.css'
+
+        If (-Not $ContainsApplicationFiles){
+            Throw "Not a web app"
+        }Else {
+            Write-Host "Source files looks good, continuing"
+        }
+    }
+    Catch {
+        Throw "No backup created due to : $($_.Exception.Message)"
+    }
+}
 #Checks if the source Path exists and if not then it returns the message
 If(-Not (Test-Path $Path))
 {
     Throw "The source directory $Path does not exist"
-}
+}s
 $date = Get-Date -format "yyyy-MM-dd"
 $DestinationFile = "$($DestinationPath + 'backup-' + $date + '.zip')"
 If (-Not (Test-Path $DestinationFile))
